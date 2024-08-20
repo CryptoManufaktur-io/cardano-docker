2f73b9debc6888d5a6dc5fe288a58b760c90994d
George Gatheca <ggatheca@cryptomanufaktur.io>
2024-08-21 00:32:07 +0100

    new cardano node

---
+++ config/preprod/update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+curl -O -J https://book.world.dev.cardano.org/environments/preprod/config.json
+curl -O -J https://book.world.dev.cardano.org/environments/preprod/db-sync-config.json
+curl -O -J https://book.world.dev.cardano.org/environments/preprod/submit-api-config.json
+curl -O -J https://book.world.dev.cardano.org/environments/preprod/topology.json
+curl -O -J https://book.world.dev.cardano.org/environments/preprod/byron-genesis.json
+curl -O -J https://book.world.dev.cardano.org/environments/preprod/shelley-genesis.json
+curl -O -J https://book.world.dev.cardano.org/environments/preprod/alonzo-genesis.json
+curl -O -J https://book.world.dev.cardano.org/environments/preprod/conway-genesis.json
---
+++ config/preview/update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+curl -O -J https://book.world.dev.cardano.org/environments/preview/config.json
+curl -O -J https://book.world.dev.cardano.org/environments/preview/db-sync-config.json
+curl -O -J https://book.world.dev.cardano.org/environments/preview/submit-api-config.json
+curl -O -J https://book.world.dev.cardano.org/environments/preview/topology.json
+curl -O -J https://book.world.dev.cardano.org/environments/preview/byron-genesis.json
+curl -O -J https://book.world.dev.cardano.org/environments/preview/shelley-genesis.json
+curl -O -J https://book.world.dev.cardano.org/environments/preview/alonzo-genesis.json
+curl -O -J https://book.world.dev.cardano.org/environments/preview/conway-genesis.json
