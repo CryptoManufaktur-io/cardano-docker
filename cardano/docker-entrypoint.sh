a32a3407ca070550da50cef9dcae70c275ac8d6c
George Gatheca <gatheca.george@gmail.com>
2024-10-01 11:43:46 +0100

    Add support for snapshot

--- cardano/docker-entrypoint.sh
+++ cardano/docker-entrypoint.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
 set -eu
 
+# Check if DB_DIR exists and is empty, and if SNAPSHOT is set and not empty
+if [[ -d "${DB_DIR}" && -z "$(ls -A ${DB_DIR})" ]] && [[ -n "${SNAPSHOT}" ]]; then
+    mkdir -p /runtime/temp
+    FILENAME="/runtime/temp/snapshot.tar.lz4"
+    
+    echo "Downloading snapshot from ${SNAPSHOT}..."
+    wget -O "${FILENAME}" "${SNAPSHOT}" --progress=dot:giga
+
+    # Check if the file was downloaded successfully
+    if [[ -f "${FILENAME}" ]]; then
+        echo "Extracting ${FILENAME} to ${DB_DIR}..."
+        lz4 -dvc --no-sparse "${FILENAME}" | tar xv -C "${DB_DIR}"
+
+        # Check if the extraction was successful
+        if [[ $? -eq 0 ]]; then
+            echo "Extraction for snapshot successful!"
+            rm -rf /runtime/temp
+        else
+            echo "Error: Extraction for snapshot failed."
+            exit 1
+        fi
+    else
+        echo "Error: Failed to download snapshot file."
+        exit 1
+    fi
+else
+    echo "No snapshot provided or already synced"
+fi
+
+
 # Check if KES is not empty
 if [[ -n "$KES" ]]; then
     cardano-node run \
